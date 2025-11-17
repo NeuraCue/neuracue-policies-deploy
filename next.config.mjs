b63a4bb77862bd59dbb2bd9066546a78ec51ecfb
Jon DeMaagd <jdemaagd4@neuracue.com>
2025-11-16 18:45:05 -0800

    minimal config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+const nextConfig = {}
+module.exports = nextConfig
